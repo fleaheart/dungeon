53f3e63cd82e76172b84ae090cf40cba64d5d3b6
user <user@local>
2016-11-27 21:40:57 +0900

    TextAdv移植完了

--- sandbox/TextAdv.js
+++ sandbox/TextAdv.js
@@ -1,18 +1,181 @@
+var MODE_MAKIMONO = 'makimono';
+var MODE_KAMISHIBAI = 'kamishibai';
 var TextAdv = (function () {
-    function TextAdv() {
-        this.MODE_MAKIMONO = 'makimono';
-        this.MODE_KAMISHIBAI = 'kamishibai';
+    function TextAdv(display, scene) {
+        this.display = display;
+        this.scene = scene;
+        this.linkColor = 'blue';
+        this.selectColor = 'red';
+        this.currentStep = null;
+        this.step = 1;
+        this.trace = new Array();
+        this.mode = MODE_MAKIMONO;
+        this.interval = 100;
+        this.dy = 10;
     }
-    TextAdv.prototype.test = function () {
-        var i = 0;
-        var c = 99;
-        i = 2;
+    TextAdv.prototype.start = function () {
+        this.step = 1;
+        this.go(0);
+    };
+    TextAdv.prototype.go = function (idx, selectedElem) {
+        if (selectedElem != null) {
+            var parent_1 = null;
+            if (this.mode == MODE_MAKIMONO) {
+                parent_1 = this.searchUpperElemnt(selectedElem, 'scene');
+            }
+            else {
+                parent_1 = this.display;
+            }
+            var elems = new Array();
+            this.pickupElements(parent_1, 'link', elems);
+            for (var i = 0; i < elems.length; i++) {
+                elems[i].style.color = this.linkColor;
+            }
+            selectedElem.style.color = this.selectColor;
+        }
+        // 次に表示する用にすでに表示しているものを消す
         {
-            var a = 2;
-            a = a + 1;
+            var i = this.step;
+            while (true) {
+                var elem = document.getElementById('sc' + i);
+                if (elem == null) {
+                    break;
+                }
+                elem.parentNode.removeChild(elem);
+                i++;
+            }
+        }
+        var html = '';
+        {
+            var bodyParts = this.scene[idx].split('◇');
+            if (2 <= bodyParts.length) {
+                document.title = bodyParts[0];
+                html = bodyParts[1];
+            }
+            else {
+                html = this.scene[idx];
+            }
+        }
+        while (true) {
+            var s = html.indexOf('[', 0), e = html.indexOf(']', 0);
+            var before = null;
+            var linkParts = null;
+            if (0 <= s && 0 <= e) {
+                before = html.substring(s, e + 1);
+                linkParts = html.substring(s + 1, e).split('→');
+                if (linkParts.length != 2) {
+                    linkParts = html.substring(s + 1, e).split('←');
+                    if (linkParts.length == 2) {
+                        var swap = linkParts[0];
+                        linkParts[0] = linkParts[1];
+                        linkParts[1] = swap;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+            else {
+                var cc = 2;
+                s = html.indexOf('→', 0);
+                if (0 <= s) {
+                    e = s + 1;
+                    while (e < html.length) {
+                        var c = html.charAt(e);
+                        if (isNaN(Number(c)) || c == ' ') {
+                            break;
+                        }
+                        e++;
+                    }
+                    before = html.substring(s, e);
+                    var linkNum = html.substring(s + 1, e);
+                    linkParts = new Array();
+                    linkParts[0] = '⇒' + linkNum;
+                    linkParts[1] = linkNum;
+                }
+                else {
+                    break;
+                }
+            }
+            if (linkParts == null) {
+                break;
+            }
+            var after = '<span class="link" onclick="go(' + linkParts[1] + ', this);">' + linkParts[0] + '</span>';
+            html = html.replace(before, after);
+        }
+        while (0 <= html.indexOf('⇒', 0)) {
+            html = html.replace('⇒', '→');
+        }
+        var id = null;
+        if (this.mode == MODE_MAKIMONO) {
+            id = 'sc' + this.step;
+            var div = '<div id="' + id + '" class="scene">' + html + '</div><p>';
+            var r = document.createRange();
+            r.selectNode(this.display);
+            this.display.appendChild(r.createContextualFragment(div));
+            (function (xthis, step) {
+                document.getElementById(id).addEventListener('mouseover', function () {
+                    xthis.step = step + 1;
+                });
+            })(this, this.step);
+            this.step++;
+        }
+        else if (this.mode == MODE_KAMISHIBAI) {
+            id = this.display.id;
+            this.display.innerHTML = html;
+            this.step++;
+        }
+        this.trace.push(idx);
+        {
+            var elems = new Array();
+            this.pickupElements(document.getElementById(id), 'link', elems);
+            for (var i = 0; i < elems.length; i++) {
+                elems[i].style.color = this.linkColor;
+                elems[i].style.textDecoration = 'underline';
+                elems[i].style.cursor = 'pointer';
+            }
+        }
+        if (this.mode == MODE_MAKIMONO) {
+        }
+    };
+    TextAdv.prototype.back = function () {
+        this.trace.pop();
+        var idx = this.trace.pop();
+        if (idx != null) {
+            this.go(idx);
+        }
+    };
+    TextAdv.prototype.searchUpperElemnt = function (elem, className) {
+        var parent = elem.parentNode;
+        if (parent == null) {
+            return null;
+        }
+        if (parent.className == className) {
+            return parent;
+        }
+        return this.searchUpperElemnt(parent, className);
+    };
+    TextAdv.prototype.pickupElements = function (parentElem, className, pickupElems) {
+        if (pickupElems == null) {
+            return;
+        }
+        var childElems = parentElem.childNodes;
+        for (var i = 0; i < childElems.length; i++) {
+            var elem = childElems.item(i);
+            if (0 < elem.childNodes.length) {
+                this.pickupElements(elem, className, pickupElems);
+            }
+            if (elem.className == className) {
+                pickupElems.push(elem);
+            }
+        }
+    };
+    TextAdv.prototype.scroll = function () {
+        if (document.body.clientHeight + window.pageYOffset < document.body.scrollHeight) {
+            window.scrollBy(0, 10);
+            setTimeout(arguments.callee, 1);
         }
     };
     return TextAdv;
 }());
-;
 //# sourceMappingURL=TextAdv.js.map
--- sandbox/TextAdv.ts
+++ sandbox/TextAdv.ts
@@ -1,20 +1,212 @@
 
+const MODE_MAKIMONO: string = 'makimono';
+const MODE_KAMISHIBAI: string = 'kamishibai';
+
 class TextAdv {
+    public linkColor: string = 'blue';
+    public selectColor: string = 'red';
 
-    public MODE_MAKIMONO :string = 'makimono';
-    public MODE_KAMISHIBAI :string = 'kamishibai';
+    private currentStep: number = null;
+    private step: number = 1;
+    private trace: number[] = new Array();
+    private mode: string = MODE_MAKIMONO;
 
-    test () {
-        let i: number  = 0;
-        const c = 99;
+    constructor(public display: HTMLElement, public scene: string[]) {
 
-        i = 2;
+    }
 
-        
+    public start(): void {
+        this.step = 1;
+        this.go(0);
+    }
+
+    public go(idx: number, selectedElem?: HTMLElement) {
+        if (selectedElem != null) {
+            let parent: HTMLElement = null;
+            if (this.mode == MODE_MAKIMONO) {
+                parent = this.searchUpperElemnt(selectedElem, 'scene');
+            } else {
+                parent = this.display;
+            }
+
+            let elems: HTMLElement[] = new Array();
+            this.pickupElements(parent, 'link', elems);
+
+            for (let i: number = 0; i < elems.length; i++) {
+                elems[i].style.color = this.linkColor;
+            }
+            selectedElem.style.color = this.selectColor;
+        }
+
+        // 次に表示する用にすでに表示しているものを消す
         {
-        let a = 2;
-        a = a + 1;
+            let i: number = this.step;
+            while (true) {
+                let elem: HTMLElement = document.getElementById('sc' + i);
+                if (elem == null) {
+                    break;
+                }
+                elem.parentNode.removeChild(elem);
+
+                i++;
+            }
+        }
+
+        let html: string = '';
+        {
+            let bodyParts: string[] = this.scene[idx].split('◇');
+            if (2 <= bodyParts.length) {
+                document.title = bodyParts[0];
+                html = bodyParts[1];
+            } else {
+                html = this.scene[idx];
+            }
+        }
+
+        while (true) {
+            let s: number = html.indexOf('[', 0),
+                e: number = html.indexOf(']', 0);
+
+            let before: string = null;
+            let linkParts: string[] = null;
+
+            if (0 <= s && 0 <= e) {
+                before = html.substring(s, e + 1);
+                linkParts = html.substring(s + 1, e).split('→');
+
+                if (linkParts.length != 2) {
+                    linkParts = html.substring(s + 1, e).split('←');
+                    if (linkParts.length == 2) {
+                        let swap = linkParts[0];
+                        linkParts[0] = linkParts[1];
+                        linkParts[1] = swap;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                s = html.indexOf('→', 0);
+                if (0 <= s) {
+                    e = s + 1;
+                    while (e < html.length) {
+                        let c = html.charAt(e);
+                        if (isNaN(Number(c)) || c == ' ') {
+                            break;
+                        }
+                        e++;
+                    }
+                    before = html.substring(s, e);
+                    let linkNum: string = html.substring(s + 1, e);
+                    linkParts = new Array();
+                    linkParts[0] = '⇒' + linkNum;
+                    linkParts[1] = linkNum;
+                } else {
+                    break;
+                }
+            }
+
+            if (linkParts == null) {
+                break;
+            }
+
+            let after: string = '<span class="link" onclick="go(' + linkParts[1] + ', this);">' + linkParts[0] + '</span>';
+
+            html = html.replace(before, after);
+        }
+
+        while (0 <= html.indexOf('⇒', 0)) {
+            html = html.replace('⇒', '→');
+        }
+
+        let id: string = null;
+
+        if (this.mode == MODE_MAKIMONO) {
+            id = 'sc' + this.step;
+            let div: string = '<div id="' + id + '" class="scene">' + html + '</div><p>';
+            let r = document.createRange();
+            r.selectNode(this.display);
+            this.display.appendChild(r.createContextualFragment(div));
+
+            (function (xthis, step) {
+                document.getElementById(id).addEventListener('mouseover', function () {
+                    xthis.step = step + 1;
+                });
+            })(this, this.step);
+
+            this.step++;
+
+        } else if (this.mode == MODE_KAMISHIBAI) {
+            id = this.display.id;
+            this.display.innerHTML = html;
+            this.step++;
+        }
+
+        this.trace.push(idx);
+
+        {
+            let elems: HTMLElement[] = new Array();
+            this.pickupElements(document.getElementById(id), 'link', elems);
+            for (let i: number = 0; i < elems.length; i++) {
+                elems[i].style.color = this.linkColor;
+                elems[i].style.textDecoration = 'underline';
+                elems[i].style.cursor = 'pointer';
+            }
+        }
+
+        if (this.mode == MODE_MAKIMONO) {
+            //    this.scroll();
         }
     }
-};
 
+    public back() {
+        this.trace.pop();
+        let idx: number = this.trace.pop();
+
+        if (idx != null) {
+            this.go(idx);
+        }
+    }
+
+    private searchUpperElemnt(elem: HTMLElement, className: string): HTMLElement {
+        let parent: HTMLElement = <HTMLElement>elem.parentNode;
+        if (parent == null) {
+            return null;
+        }
+
+        if (parent.className == className) {
+            return parent;
+        }
+
+        return this.searchUpperElemnt(parent, className);
+    }
+
+    private pickupElements(parentElem: HTMLElement, className: string, pickupElems: HTMLElement[]): void {
+        if (pickupElems == null) {
+            return;
+        }
+
+        let childElems: NodeList = parentElem.childNodes;
+        for (let i: number = 0; i < childElems.length; i++) {
+            let elem: HTMLElement = <HTMLElement>childElems.item(i);
+
+            if (0 < elem.childNodes.length) {
+                this.pickupElements(elem, className, pickupElems);
+            }
+
+            if (elem.className == className) {
+                pickupElems.push(elem);
+            }
+        }
+    }
+
+    private interval: number = 100;
+    private dy: number = 10;
+
+    public scroll(): void {
+        if (document.body.clientHeight + window.pageYOffset < document.body.scrollHeight) {
+            window.scrollBy(0, 10);
+            setTimeout(arguments.callee, 1);
+        }
+    }
+
+}
